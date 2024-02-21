ad902fe5602bf1821ffdfdca56bc777bbfda4f55
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-21 18:34:04 +0900

    feat: 위키 리스트 렌더링 기능 구현

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,29 @@
-export default function Main() {
-  // const wikiList = [];
+import { Link } from 'react-router-dom';
 
-  return <div className='text-3xl font-bold'>메인</div>;
+export default function Main() {
+  const wikiList = [
+    {
+      id: 0,
+      title: '제목1',
+      contents: '본문1',
+    },
+    {
+      id: 1,
+      title: '제목2',
+      contents: '본문2',
+    },
+    {
+      id: 2,
+      title: '제목3',
+      contents: '본문3',
+    },
+  ];
+
+  return (
+    <ul className='flex h-40 flex-col items-center justify-center bg-sky-500'>
+      {wikiList.map(({ title }) => {
+        return <Link to='wiki'>{title}</Link>;
+      })}
+    </ul>
+  );
 }

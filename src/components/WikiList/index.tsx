3e757b39592a8b7fd809e28dfc0bddd63a3b3742
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-22 17:29:53 +0900

    refactor: `Main` 페이지에서 `WikiList` 분리

---
+++ src/components/WikiList/index.tsx
@@ -0,0 +1,23 @@
+import { useRecoilValue } from 'recoil';
+import { Link } from 'react-router-dom';
+
+import { wikiListState } from '../../states/wikiList';
+import { PATHS } from '../../constants/route';
+
+export default function WikiList() {
+  const wikiList = useRecoilValue(wikiListState);
+
+  if (!wikiList.length) return <div>위키가 존재하지 않아요.</div>;
+
+  return (
+    <ul className='flex h-40 flex-col items-center justify-center bg-sky-500'>
+      {wikiList.map(({ id, title }) => {
+        return (
+          <li key={id}>
+            <Link to={PATHS.WIKI}>{title}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,31 +1,16 @@
 import { Link } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 
 import { PATHS } from '../../constants/route';
-import { wikiListState } from '../../states/wikiList/atom';
+import WikiList from '../../components/WikiList';
 
 export default function Main() {
-  const wikiList = useRecoilValue(wikiListState);
-
   return (
-    <>
+    <div>
       <Link to={PATHS.WIKI_CREATOR} target='_blank'>
         새로운 위키 추가하기
       </Link>
 
-      {wikiList.length ? (
-        <ul className='flex h-40 flex-col items-center justify-center bg-sky-500'>
-          {wikiList.map(({ id, title }) => {
-            return (
-              <li key={id}>
-                <Link to={PATHS.WIKI}>{title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <div>위키가 존재하지 않아요.</div>
-      )}
-    </>
+      <WikiList />
+    </div>
   );
 }

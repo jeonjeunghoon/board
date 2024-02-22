c8f46333f41d7c5cb6a5daa8e4c7835c79c5444b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-22 14:01:21 +0900

    chore: recoil 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
-  return <Outlet />;
+  return (
+    <RecoilRoot>
+      <Outlet />
+    </RecoilRoot>
+  );
 }
 
 export default App;

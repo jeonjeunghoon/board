2d343ca68ba6f638c219e678e17b6f15982f3a16
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-23 23:18:38 +0900

    design: `WikiList` 디자인 적용

--- src/components/WikiList/index.tsx
+++ src/components/WikiList/index.tsx
@@ -10,16 +10,19 @@ export default function WikiList({ wikiList }: Props) {
   if (!wikiList.length) return <div>위키가 존재하지 않아요.</div>;
 
   return (
-    <div>
-      <ul className='flex h-40 flex-col items-center justify-center bg-sky-500'>
-        {wikiList.map(({ id, title }) => {
-          return (
-            <li key={id}>
-              <Link to={String(id)}>{title}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul>
+      {wikiList.map(({ id, title }) => {
+        return (
+          <li key={id}>
+            <Link
+              className='hover:co hover:text block w-full rounded-lg px-6 py-8 text-2xl font-thin hover:bg-gray-100 hover:text-blue-700'
+              to={String(id)}
+            >
+              {title}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 }

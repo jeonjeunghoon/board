2ccb61b8e9fbf8b24bf2f3666cacf9a982565432
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-23 22:54:42 +0900

    feat: `Header` 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+import { PATHS } from '../../constants/routes';
+
+export default function Header() {
+  return (
+    <header className='flex h-14 justify-between px-14 py-4'>
+      <p className='cursor-default text-xl font-bold'>코딩허브 전증훈</p>
+      <Link className='text-base font-medium' to={PATHS.WIKI.MAIN}>
+        위키 게시판
+      </Link>
+    </header>
+  );
+}

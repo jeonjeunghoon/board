7a032a53eb0f4b95ac3323ea3bfc609aecdc9057
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-23 23:39:35 +0900

    design: theme 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      black: '#333D4B',
+      primaryText: '#FFFFFF',
+      primaryTextHover: '#FFFFFF',
+      secondaryText: '#6B7684',
+      secondaryTextHover: '#0056B3',
+    },
+
+    backgroundColor: {
+      primaryTextBackground: '#307FFF',
+      primaryTextBackgroundHover: '#1B64D9',
+      secondaryTextBackground: '#FFFFFF',
+      secondaryTextBackgroundHover: '#EAF3FF',
+      secondaryTextBackgroundFocus: '#E5E8EA',
+    },
   },
   plugins: [],
 };

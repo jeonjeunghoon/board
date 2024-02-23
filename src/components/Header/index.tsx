acf6e18e3e464223da6876009e94f5cfbea0fba1
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-23 22:59:43 +0900

    design: `Header` 보더 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { PATHS } from '../../constants/routes';
 
 export default function Header() {
   return (
-    <header className='flex h-14 justify-between px-14 py-4'>
+    <header className='flex h-14 justify-between border-b px-14 py-4'>
       <p className='cursor-default text-xl font-bold'>코딩허브 전증훈</p>
       <Link className='text-base font-medium' to={PATHS.WIKI.MAIN}>
         위키 게시판

dada22059b2d1bb72d321d5bb7caaaa355b0e918
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-25 19:03:12 +0900

    chore: storybook router, recoil 설정

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,22 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
+import { withRouter, reactRouterParameters } from 'storybook-addon-react-router-v6';
+import { withRecoilFlow } from 'storybook-addon-recoil-flow/dist/decorator';
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    reactRouter: reactRouterParameters({
+      routing: { path: '/wiki' },
+    }),
   },
+  decorators: [withRouter, withRecoilFlow],
 };
 
 export default preview;
